7231e574deb29faa1879d1301f4945a7ef886523
Mark Nelson <markn@moodle.com>
2016-07-11 18:48:51 +0800

    MDL-54687 core_message: added ability to delete conversations

--- message/classes/api.php
+++ message/classes/api.php
@@ -158,4 +158,101 @@ class api {
             return new \core_message\output\profile($userid, $data);
         }
     }
+
+    /**
+     * Checks if a user can delete messages they have either received or sent.
+     *
+     * @param int $userid The user id of who we want to delete the messages for (this may be done by the admin
+     *  but will still seem as if it was by the user)
+     * @return bool Returns true if a user can delete the message, false otherwise.
+     */
+    public static function can_delete_conversation($userid) {
+        global $USER;
+
+        $systemcontext = \context_system::instance();
+
+        // Let's check if the user is allowed to delete this message.
+        if (has_capability('moodle/site:deleteanymessage', $systemcontext) ||
+            ((has_capability('moodle/site:deleteownmessage', $systemcontext) &&
+                $USER->id == $userid))) {
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * Deletes a conversation.
+     *
+     * This function does not verify any permissions.
+     *
+     * @param int $userid The user id of who we want to delete the messages for (this may be done by the admin
+     *  but will still seem as if it was by the user)
+     * @param int $otheruserid The id of the other user in the conversation
+     * @return bool
+     */
+    public static function delete_conversation($userid, $otheruserid) {
+        global $DB, $USER;
+
+        // We need to update the tables to mark all messages as deleted from and to the other user. This seems worse than it
+        // is, that's because our DB structure splits messages into two tables (great idea, huh?) which causes code like this.
+        // This won't be a particularly heavily used function (at least I hope not), so let's hope MDL-36941 gets worked on
+        // soon for the sake of any developers' sanity when dealing with the messaging system.
+        $now = time();
+        $sql = "UPDATE {message}
+               SET timeuserfromdeleted = :time
+             WHERE useridfrom = :userid
+               AND useridto = :otheruserid";
+        $DB->execute($sql, array('time' => $now, 'userid' => $userid, 'otheruserid' => $otheruserid));
+
+        $sql = "UPDATE {message}
+               SET timeusertodeleted = :time
+             WHERE useridto = :userid
+               AND useridfrom = :otheruserid";
+        $DB->execute($sql, array('time' => $now, 'userid' => $userid, 'otheruserid' => $otheruserid));
+
+        $sql = "UPDATE {message_read}
+               SET timeuserfromdeleted = :time
+             WHERE useridfrom = :userid
+               AND useridto = :otheruserid";
+        $DB->execute($sql, array('time' => $now, 'userid' => $userid, 'otheruserid' => $otheruserid));
+
+        $sql = "UPDATE {message_read}
+               SET timeusertodeleted = :time
+             WHERE useridto = :userid
+               AND useridfrom = :otheruserid";
+        $DB->execute($sql, array('time' => $now, 'userid' => $userid, 'otheruserid' => $otheruserid));
+
+        // Now we need to trigger events for these. BAH - another SQL query.
+        $sql = "SELECT id, useridfrom, useridto, subject, fullmessage, fullmessagehtml, fullmessageformat,
+                       smallmessage, notification, timecreated, 0 as timeread
+                  FROM {message} m
+                 WHERE (useridto = ? AND useridfrom = ? AND timeusertodeleted = ?)
+                    OR (useridto = ? AND useridfrom = ? AND timeuserfromdeleted = ?)
+             UNION ALL
+                SELECT id, useridfrom, useridto, subject, fullmessage, fullmessagehtml, fullmessageformat,
+                       smallmessage, notification, timecreated, timeread
+                  FROM {message_read} mr
+                 WHERE (useridto = ? AND useridfrom = ? AND timeusertodeleted = ?)
+                    OR (useridto = ? AND useridfrom = ? AND timeuserfromdeleted = ?)";
+        $params = array($userid, $otheruserid, $now,
+                        $otheruserid, $userid, $now,
+                        $userid, $otheruserid, $now,
+                        $otheruserid, $userid, $now);
+        if ($messages = $DB->get_records_sql($sql, $params)) {
+            // Loop through and mark them as deleted.
+            foreach ($messages as $message) {
+                $messagetable = 'message';
+                if (!empty($message->timeread)) {
+                    $messagetable = 'message_read';
+                }
+
+                // Trigger event for deleting the message.
+                \core\event\message_deleted::create_from_ids($message->useridfrom, $message->useridto,
+                    $USER->id, $messagetable, $message->id)->trigger();
+            }
+        }
+
+        return true;
+    }
 }
