ad6966003ae7ce4bccfcff2f7bfe0908355607e8
Mark Nelson <markn@moodle.com>
2016-08-16 18:21:17 +0800

    MDL-54698 core_message: moved 'disable/enable' notifications setting

--- lib/outputrenderers.php
+++ lib/outputrenderers.php
@@ -3199,7 +3199,6 @@ EOD;
         if (isloggedin() && $processor->enabled) {
             $context = [
                 'userid' => $USER->id,
-                'allnotificationsdisabled' => !empty($USER->emailstop),
                 'urls' => [
                     'preferences' => (new moodle_url('/message/notificationpreferences.php', ['id' => $USER->id]))->out(),
                 ],
