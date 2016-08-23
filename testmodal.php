e464a99553a4b731e696a5a9709b8b0ea3beea2f
Ryan Wyllie <ryan@moodle.com>
2016-08-23 03:33:44 +0000

    TEST COMMIT PLEASE IGNORE

---
+++ testmodal.php
@@ -0,0 +1,39 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * This page allows the configuration of external tools that meet the LTI specification.
+ *
+ * @package    mod_lti
+ * @copyright  2015 Ryan Wyllie <ryan@moodle.com>
+ * @author     Ryan Wyllie
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+require_once('./config.php');
+
+// No guest autologin.
+require_login(0, false);
+
+$pageurl = new moodle_url('/testmodal.php');
+$PAGE->set_url($pageurl);
+$PAGE->set_title("Test Modal");
+
+$output = $PAGE->get_renderer('core');
+
+echo $output->header();
+echo $output->render_from_template('core/test_modal_page', []);
+echo $output->footer();
