1011cd8536d4d14dafc9e7e0dba9f5ad9ea1d45e
Mark Nelson <markn@moodle.com>
2016-12-21 16:51:41 +0800

    MDL-57434 core: deprecated xxx_print_overview() and related functions
    Part of MDL-55611.

--- course/externallib.php
+++ course/externallib.php
@@ -2645,6 +2645,8 @@ class core_course_external extends external_api {
     /**
      * Returns description of method parameters
      *
+     * @deprecated since 3.3
+     *
      * @return external_function_parameters
      * @since Moodle 3.2
      */
@@ -2658,6 +2660,8 @@ class core_course_external extends external_api {
 
     /**
      * Return activities overview for the given courses.
+     *
+     * @deprecated since 3.3
      *
      * @param array $courseids a list of course ids
      * @return array of warnings and the activities overview
@@ -2716,6 +2720,8 @@ class core_course_external extends external_api {
     /**
      * Returns description of method result value
      *
+     * @deprecated since 3.3
+     *
      * @return external_description
      * @since Moodle 3.2
      */
@@ -2740,6 +2746,15 @@ class core_course_external extends external_api {
                 'warnings' => new external_warnings()
             )
         );
+    }
+
+    /**
+     * Marking the method as deprecated.
+     *
+     * @return bool
+     */
+    public static function get_activities_overview_is_deprecated() {
+        return true;
     }
 
     /**
