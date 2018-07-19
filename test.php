ba76b1d53ec39f8f7e10ce30aeb4a1976fdd66ba
Ryan Wyllie <ryan@ryanwyllie.com>
2018-07-19 13:18:20 +0800

    fix page title test.php

--- test.php
+++ test.php
@@ -4,7 +4,7 @@ require_once('./config.php');
 
 require_login(0, false);
 $PAGE->set_context(context_system::instance());
-$PAGE->set_url('/test_paged_content.php', []);
+$PAGE->set_url('/test.php', []);
 $PAGE->set_pagelayout('admin');
 $PAGE->set_title("Test page");
 $PAGE->set_heading("Test page");
