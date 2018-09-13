f54cdcf3727cc34be466d7192bd3a41ddac654f3
Ryan Wyllie <ryan@moodle.com>
2018-09-13 09:35:52 +0800

    add search view to drawer

--- message/output/popup/amd/src/message_drawer.js
+++ message/output/popup/amd/src/message_drawer.js
@@ -29,6 +29,7 @@ define(
     'message_popup/message_drawer_view_contact',
     'message_popup/message_drawer_view_overview',
     'message_popup/message_drawer_view_conversation',
+    'message_popup/message_drawer_view_search',
     'message_popup/message_router',
     'message_popup/message_routes'
 ],
@@ -38,6 +39,7 @@ function(
     ViewContact,
     ViewOverview,
     ViewConversation,
+    ViewSearch,
     Router,
     Routes
 ) {
@@ -51,6 +53,7 @@ function(
         VIEW_NON_CONTACT: '[data-region="view-non-contact"]',
         VIEW_OVERVIEW: '[data-region="view-overview"]',
         VIEW_REQUESTS: '[data-region="view-requests"]',
+        VIEW_SEARCH: '[data-region="view-search"]',
         VIEW_SETTINGS: '[data-region="view-settings"]',
         ROUTES: '[data-route]',
         ROUTES_BACK: '[data-route-back]'
@@ -65,6 +68,7 @@ function(
         Router.add(Routes.VIEW_NON_CONTACT, root.find(SELECTORS.VIEW_NON_CONTACT));
         Router.add(Routes.VIEW_OVERVIEW, root.find(SELECTORS.VIEW_OVERVIEW), ViewOverview.show);
         Router.add(Routes.VIEW_REQUESTS, root.find(SELECTORS.VIEW_REQUESTS));
+        Router.add(Routes.VIEW_SEARCH, root.find(SELECTORS.VIEW_SEARCH), ViewSearch.show);
         Router.add(Routes.VIEW_SETTINGS, root.find(SELECTORS.VIEW_SETTINGS));
     };
 
