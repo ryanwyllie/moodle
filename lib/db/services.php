7ea12d350a2c57f183956977bfba04652b2cf275
Ryan Wyllie <ryan@moodle.com>
2018-11-05 16:11:26 +0800

    MDL-63303 message: add get_conversation to message api

--- lib/db/services.php
+++ lib/db/services.php
@@ -1046,6 +1046,15 @@ $functions = array(
         'services' => array(MOODLE_OFFICIAL_MOBILE_SERVICE),
         'ajax' => true
     ),
+    'core_message_get_conversation' => array(
+        'classname' => 'core_message_external',
+        'methodname' => 'get_conversation',
+        'classpath' => 'message/externallib.php',
+        'description' => 'Retrieve a conversation for a user',
+        'type' => 'read',
+        'services' => array(MOODLE_OFFICIAL_MOBILE_SERVICE),
+        'ajax' => true
+    ),
     'core_message_get_messages' => array(
         'classname' => 'core_message_external',
         'methodname' => 'get_messages',
