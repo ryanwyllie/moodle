0242e112ac43588f6d2b418c910a2f3bf31d2a53
Andrew Nicols <andrew@nicols.co.uk>
2019-07-09 09:35:22 +0800

    MDL-62497 javascript: lazy load js modules when cachejs is disabled

--- lib/jssourcemap.php
+++ lib/jssourcemap.php
@@ -28,7 +28,6 @@ define('NO_DEBUG_DISPLAY', true);
 
 require('../config.php');
 require_once("$CFG->dirroot/lib/configonlylib.php");
-require_once("$CFG->dirroot/lib/jslib.php");
 require_once("$CFG->dirroot/lib/classes/requirejs.php");
 
 $slashargument = min_get_slash_argument();
@@ -38,17 +37,11 @@ if (!$slashargument) {
 }
 
 $slashargument = ltrim($slashargument, '/');
-if (substr_count($slashargument, '/') < 1) {
-    header('HTTP/1.0 404 not found');
-    die('Slash argument must contain both a revision and a file path');
-}
 // Split into revision and module name.
-[$hash, $rev, $file] = explode('/', $slashargument, 3);
-$rev  = min_clean_param($rev, 'INT');
+[$file] = explode('/', $slashargument, 1);
 $file = '/' . min_clean_param($file, 'SAFEPATH');
 
 // Only load js files from the js modules folder from the components.
-$jsfiles = array();
 [$unused, $component, $module] = explode('/', $file, 3);
 
 // No subdirs allowed - only flat module structure please.
@@ -56,108 +49,24 @@ if (strpos('/', $module) !== false) {
     die('Invalid module');
 }
 
-$cache = \cache::make('core', 'javascript_source_map');
-$md5file = md5($file);
-$hashcachekey = "hash_{$md5file}";
-$sourcemapcachekey = "source_map_{$md5file}";
-$lasthash = $cache->get($hashcachekey);
+// When running a lazy load, we only deal with one file so we can just return the working sourcemap.
+$jsfiles = core_requirejs::find_one_amd_module($component, $module, false);
+$jsfile = reset($jsfiles);
 
-if ($lasthash !== false && $lasthash === $hash) {
-    // The JS content hasn't changed (same hash that we last saw) so let's see
-    // if we've already got a source map for that file.
-    $existingsourcemap = $cache->get($sourcemapcachekey);
-    if ($existingsourcemap !== false) {
-        // We've got an existing source map so let's use that.
-        js_send_uncached($existingsourcemap, 'jssourcemap.php');
-    }
-}
+$mapfile = $jsfile . '.map';
 
-// Some (huge) modules are better loaded lazily (when they are used). If we are requesting
-// one of these modules, only return the one module, not the combo.
-$lazysuffix = "-lazy.js";
-$lazyload = (strpos($module, $lazysuffix) !== false);
+if (file_exists($mapfile)) {
+    $mapdata = file_get_contents($mapfile);
+    $mapdata = json_decode($mapdata, true);
 
-if ($lazyload) {
-    $jsfiles = core_requirejs::find_one_amd_module($component, $module, false);
-} else {
-    $jsfiles = core_requirejs::find_all_amd_modules(false);
-}
-
-// Create the empty source map.
-$map = [
-    'version' => 3,
-    'file' => $CFG->wwwroot . '/lib/requirejs.php/' . $slashargument,
-    'sections' => []
-];
-
-$line = 0;
-// Sort the files to ensure consistent ordering for source map generation.
-asort($jsfiles);
-
-foreach ($jsfiles as $modulename => $jsfile) {
     $shortfilename = str_replace($CFG->dirroot, '', $jsfile);
     $srcfilename = str_replace('/amd/build/', '/amd/src/', $shortfilename);
     $srcfilename = str_replace('.min.js', '.js', $srcfilename);
     $fullsrcfilename = $CFG->wwwroot . $srcfilename;
-    $mapfile = $jsfile . '.map';
+    $mapdata['sources'][0] = $fullsrcfilename;
 
-    if (file_exists($mapfile)) {
-        $mapdata = file_get_contents($mapfile);
-        $mapdata = json_decode($mapdata, true);
-        $mapdata['sources'][0] = $fullsrcfilename;
-
-        $map['sections'][] = [
-            'offset' => [
-                'line' => $line,
-                'column' => 0
-            ],
-            'map' => $mapdata
-        ];
-
-        $js = file_get_contents($jsfile);
-        // Remove source map link.
-        $js = preg_replace('~//# sourceMappingURL.*$~s', '', $js);
-        $js = rtrim($js);
-    } else {
-        // No sourcemap for this section which means we will have to generate a
-        // source map for it on the fly. The source map will simply be a one-to-one
-        // mapping of the original source to the map file since there is no
-        // minification.
-        $js = file_get_contents($CFG->dirroot . $srcfilename);
-        $js = rtrim($js);
-        $newmap = new axy\sourcemap\SourceMap();
-        $newmap->file = $shortfilename;
-
-        foreach (explode("\n", $js) as $index => $srcline) {
-            $newmap->addPosition([
-                'generated' => [
-                    'line' => $index,
-                    'column' => 0
-                ],
-                'source' => [
-                    'fileName' => $fullsrcfilename,
-                    'line' => $index,
-                    'column' => 0
-                ]
-            ]);
-        }
-
-        $newmap->sources->setContent($fullsrcfilename, $js);
-
-        $map['sections'][] = [
-            'offset' => [
-                'line' => $line,
-                'column' => 0
-            ],
-            'map' => $newmap->getData()
-        ];
-    }
-
-    $line += substr_count($js, "\n") + 1;
+    echo json_encode($mapdata);
+} else {
+    // If there is no source map file, then we will not generate one for you, sorry.
+    header('HTTP/1.0 404 not found');
 }
-
-$sourcemap = json_encode($map);
-// Remember to update the cache with our newly generated source map.
-$cache->set($hashcachekey, $hash);
-$cache->set($sourcemapcachekey, $sourcemap);
-js_send_uncached($sourcemap, 'jssourcemap.php');
