9d435b8a23a35abc9d05dfbc3c7aaf70d2c2db99
Ryan Wyllie <ryan@ryanwyllie.com>
2019-07-18 14:04:32 +0800

    Add simple editorjs init on course edit

--- course/format/fancy/amd/src/course_edit.js
+++ course/format/fancy/amd/src/course_edit.js
@@ -1,4 +1,3 @@
-<?php
 // This file is part of Moodle - http://moodle.org/
 //
 // Moodle is free software: you can redistribute it and/or modify
@@ -15,20 +14,16 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * Fancy course format.
+ * Module for viewing editing a course.
  *
- * @copyright 2019 Ryan Wyllie <ryan@moodle.com>
- * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @copyright  2019 Ryan Wyllie <ryan@moodle.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-defined('MOODLE_INTERNAL') || die();
+import EditorJS from 'core/editorjs';
 
-use format_fancy\local\renderers\course_edit as course_edit_renderer;
-
-if ($USER->editing) {
-    $renderer = new course_edit_renderer($PAGE->get_renderer('core'));
-    echo $renderer->render();
-    $PAGE->requires->js("/lib/editorjs/editor.min.js");
-} else {
-
+export function init(elementId) {
+    new EditorJS({
+        holderId: elementId
+    });
 }
---
+++ course/format/fancy/classes/local/renderers/course_edit.php
@@ -0,0 +1,48 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Nav bar renderer.
+ *
+ * @package    mod_forum
+ * @copyright  2019 Ryan Wyllie <ryan@moodle.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+namespace format_fancy\local\renderers;
+
+defined('MOODLE_INTERNAL') || die();
+
+use renderer_base;
+
+/**
+ * Nav bar renderer class.
+ *
+ * @copyright  2019 Ryan Wyllie <ryan@moodle.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class course_edit {
+    private $renderer;
+
+    public function __construct(renderer_base $renderer) {
+        $this->renderer = $renderer;
+    }
+
+    public function render() {
+        $renderer = $this->renderer;
+        return $renderer->render_from_template('format_fancy/course_edit', []);
+    }
+}
