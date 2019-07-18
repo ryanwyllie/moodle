7939679ae258a589b0d50e1eec1dbfb6b9f8f5a4
Ryan Wyllie <ryan@ryanwyllie.com>
2019-07-18 10:35:40 +0800

    Add course format fancy

---
+++ course/format/fancy/lib.php
@@ -0,0 +1,44 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * This file contains main class for the course format Fancy
+ *
+ * @copyright 2019 Ryan Wyllie <ryan@moodle.com>
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+require_once("{$CFG->dirroot}/course/format/lib.php");
+
+/**
+ * Main class for the Fance course format
+ *
+ * @copyright  2019 Ryan Wyllie <ryan@moodle.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class format_fancy extends format_base {
+    public function extend_course_navigation($navigation, navigation_node $node) {
+        return;
+    }
+
+    public function get_default_blocks() {
+        return [
+            BLOCK_POS_LEFT => [],
+            BLOCK_POS_RIGHT => []
+        ];
+    }
+}
