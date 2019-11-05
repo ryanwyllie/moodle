e9919d680ebce3681961570bc2f1acf1aee6c249
Ryan Wyllie <ryan@ryanwyllie.com>
2019-11-05 13:24:20 +0800

    MDL-66893 mod_forum: update navigation bar in grader UI

--- mod/forum/amd/src/local/grades/grader.js
+++ mod/forum/amd/src/local/grades/grader.js
@@ -179,7 +179,7 @@ const displayGradingError = async(root, user, err) => {
  * @param {Function} setGradeForUser A function to set the grade for a specific user
  */
 export const launch = async(getListOfUsers, getContentForUser, getGradeForUser, setGradeForUser, {
-    initialUserId = null, moduleName
+    initialUserId = null, moduleName, courseName, courseUrl
 } = {}) => {
 
     // We need all of these functions to be executed in series, if one step runs before another the interface
@@ -192,6 +192,8 @@ export const launch = async(getListOfUsers, getContentForUser, getGradeForUser, 
         createFullScreenWindow({fullscreen: false, showLoader: false}),
         Templates.renderForPromise(templateNames.grader.app, {
             moduleName,
+            courseName,
+            courseUrl,
             drawer: {show: true}
         }),
         getListOfUsers(),
